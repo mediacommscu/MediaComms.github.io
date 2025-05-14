0c21dc0b47d9c1a0bf75544a79e208d8bb0860fd
mediacommscu <sharesync.info@gmail.com>
2025-05-14 11:53:04 +0100

    Push 1

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "auto",
+    "noWording": "No",
+    "analyticsKey": "",
+    "launchAppWording": "Then Launch Project",
+    "title": "BA Media & Communication",
+    "yesWording": "Yes",
+    "downloadAppWording": "Download App",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "url": "",
+    "description": "",
+    "thumbnail": "Medias/Photos/media_comms_logo.JPG"
+}
